b19dd3305c9b7198bc022ff9af5b5af01480d3f0
Sonny Kwok <sonny.kwok@capitalone.com>
2014-03-16 23:52:06 -0400

    added web container

--- restaurants/scorsViewController.m
+++ restaurants/scorsViewController.m
@@ -18,6 +18,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *fullUrl = @"http://restaurants.scors.io";
+    NSURL *url = [NSURL URLWithString:fullUrl];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_viewWeb loadRequest:requestObj];
 }
 
 - (void)didReceiveMemoryWarning
